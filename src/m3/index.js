3e2934f16c4c99214601d125cffb2e4ccc1a6903
David Bool <davidbool@MacBool.local>
2026-03-27 12:43:53 +0300

    Restore and stabilize M3 daily execution slice

---
+++ src/m3/index.js
@@ -0,0 +1,4 @@
+export { M3ApiClient, createM3ApiClient } from "./api.js";
+export { DailyExecutionLoop } from "./dailyExecutionLoop.js";
+export { MockM3Api, createMockM3Api } from "./mockApi.js";
+export { renderTodayScreen, createTodayScreenController } from "./todayScreen.js";
